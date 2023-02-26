01045c360daaad9a2a6b7748f5df351dcbc5ffb0
cnakpil <85041294+cnakpil@users.noreply.github.com>
2023-02-26 10:10:25 -0700

    update page with brief at top.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import EntryList from './components/EntryList';
 function App() {
   return (
     <div>
+      <p>This page is an exercise to practice mapping data from json, reusable components, conditional rendering, and applying the Animate on Scroll library.</p>
+      <hr />
       <img src={headerImg} alt="Assignments" />
       <EntryList />
       <footer></footer>
