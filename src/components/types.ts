c8534050431cbd9c61687c1b4d865b218cab55a3
cnakpil <85041294+cnakpil@users.noreply.github.com>
2023-02-26 07:22:45 -0700

    update types.ts

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,23 +1,12 @@
 import React from 'react';
 
-export type CheckboxProps = {
-    key: React.Key;
-    label: React.ReactNode;
-    checked: boolean
-    handleChange: React.ChangeEventHandler<HTMLInputElement>;
-    handleClick: React.MouseEventHandler;
-}
 export type NavLinkProps = {
     children: React.ReactNode;
     href: string;
     target: string;
     className: string;
 }
-// export type EntryProps = {
-//     index: number;
-//     docLink: string;
-//     demoLink: string;
-// }
+
 export type EntryProps = {
     id: string;
     title: string;
