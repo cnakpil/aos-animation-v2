cdfc6c3f644ded691974210f78dd6729e01c6f2c
cnakpil <85041294+cnakpil@users.noreply.github.com>
2023-02-26 08:36:29 -0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import headerImg from './assets/assignments_dark.svg';
 import './App.css';
-import Entries from './components/Entries';
+// import Entries from './components/Entries';
+import EntryList from './components/EntryList';
 
 function App() {
   return (
     <div>
       <img src={headerImg} alt="Assignments" />
-      <Entries />
+      <EntryList />
     </div>
   );
 }
