8e653d91b459bab55ed8531acc0f686401f04fea
cnakpil <85041294+cnakpil@users.noreply.github.com>
2023-02-26 08:37:05 -0700

    update with entry variations

--- src/components/types.ts
+++ src/components/types.ts
@@ -11,6 +11,23 @@ export type EntryProps = {
     id: string;
     title: string;
     details: string;
-    demoLink: string
+    demoLink: string;
     docuLink: string;
+}
+export type NoDemoEntryProps = {
+    id: string;
+    title: string;
+    details: string;
+    docuLink: string;
+}
+export type NoDocuEntryProps = {
+    id: string;
+    title: string;
+    details: string;
+    demoLink: string;
+}
+export type NoLinksEntryProps = {
+    id: string;
+    title: string;
+    details: string;
 }
